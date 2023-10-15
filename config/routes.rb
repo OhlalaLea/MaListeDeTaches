e399dd364d07914ad4f70d2fd0455e320c09a06b
lea saulnier <leasaulnier@orange.fr>
2023-10-15 15:04:49 +0400

    front index show fait

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "tasks#index"
   # get 'index', to: 'tasks#index'
   resources :tasks do
     collection do
